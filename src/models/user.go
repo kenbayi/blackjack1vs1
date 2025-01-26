c7b411a928b8cf89d4bd50f146509edd16d573ba
RosShpakovskiy <231479@astanait.edu.kz>
2025-01-26 16:33:42 +0500

    LoginHandler
    LoginHandler and new function in user

--- src/models/user.go
+++ src/models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -9,4 +11,16 @@ type User struct {
 	Username  string    `json:"username"`   // Username
 	Password  string    `json:"-"`          // Hashed password (not exposed in JSON)
 	CreatedAt time.Time `json:"created_at"` // Account creation timestamp
+}
+
+func GetUserByUsername(db *sql.DB, username string) (*User, error) {
+	query := `SELECT id, username, password, created_at FROM users WHERE username = $1`
+	var user User
+	err := db.QueryRow(query, username).Scan(&user.ID, &user.Username, &user.Password, &user.CreatedAt)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, nil // User not found
+	} else if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
